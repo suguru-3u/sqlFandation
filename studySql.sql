5ebb5a08d2487acf55ae8a84e2d2efa9a1ca9a2d
Oohira Suguru <suguru.13579515@gmail.com>
2020-11-26 21:07:32 +0900

    Study sql

--- studySql.sql
+++ studySql.sql
@@ -4,3 +4,23 @@ where 日付 = '2018-02-25';
 
 delete from 家計簿
 where 日付 = '2018-02-25';
+
+select 費目 AS ITEM
+from 家計簿
+where 費目 = '給料';
+
+update 家計簿
+set 入金額 = 999;
+
+select * from 家計簿
+where 出金額 is null
+
+select * from 家計簿 
+where メモ like '%1月%'
+
+select * from 家計簿 
+where 出金額 between 1000
+and 30000
+
+select * from 家計簿
+where 費目 in('食費','交際費')
