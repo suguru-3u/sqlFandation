ddb0ccfcefe386996236f211d21cd4b177037e78
Oohira Suguru <suguru.13579515@gmail.com>
2020-11-27 07:54:33 +0900

    Study sql

--- studySql.sql
+++ studySql.sql
@@ -135,3 +135,56 @@ order by 名義
 SELECT 口座番号, 名義, 種別, 残高, 更新日
   FROM 口座
  ORDER BY 4 DESC, 1
+
+ SELECT 更新日
+  FROM 口座
+ WHERE 更新日 IS NOT NULL
+ ORDER BY 更新日
+        OFFSET 0
+ FETCH FIRST 10 ROWS ONLY
+
+ SELECT 更新日, 残高
+  FROM 口座
+ WHERE 残高 > 0
+   AND 更新日 IS NOT NULL
+ ORDER BY 残高, 更新日 DESC
+OFFSET 10
+ FETCH FIRST 10 ROWS ONLY
+
+ SELECT 口座番号
+  FROM 口座
+ UNION
+SELECT 口座番号
+  FROM 廃止口座
+ ORDER BY 1
+
+ SELECT 名義
+  FROM 口座
+EXCEPT
+SELECT 名義
+  FROM 廃止口座
+ ORDER BY 1 DESC
+
+    SELECT 名義
+     FROM 口座
+INTERSECT
+   SELECT 名義
+     FROM 廃止口座
+    ORDER BY 1
+
+SELECT 口座番号, 残高
+  FROM 口座
+ WHERE 残高 = 0
+ UNION
+SELECT 口座番号, 解約時残高
+  FROM 廃止口座
+ WHERE 解約時残高 <> 0
+ ORDER BY 1
+
+SELECT 口座番号, 名義, '○' AS 口座区分
+  FROM 口座
+ UNION
+SELECT 口座番号, 名義, '×' AS 口座区分
+  FROM 廃止口座
+ ORDER BY 名義
+
