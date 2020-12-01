3888a78ae7c5dbe58f004a26666978de938ae9a2
suguru-3u <suguru.13579515@gmail.com>
2020-12-01 22:09:23 +0900

    Study t-sql

--- t-sql.sql
+++ t-sql.sql
@@ -1,1 +1,19 @@
 print REPLACE('鈴木一郎','一','あ')
+
+print Left('aabbbc',3)
+
+print RIGHT('aaabbbc',4)
+
+print substring('aaaaccc',4,3)
+
+print upper('aaa')
+
+print lower('AAA')
+
+print trim(' aaa ')
+
+print len('aaa')
+
+print datalength('aaa')
+
+print charindex('b','aaaba',1)
