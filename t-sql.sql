40de813e542bde391241fe50573feb0f31234bd8
suguru-3u <suguru.13579515@gmail.com>
2020-12-03 07:36:29 +0900

    study t-sql

--- t-sql.sql
+++ t-sql.sql
@@ -31,3 +31,28 @@ print round(123.456,-2)
 print floor(123.456)
 
 print ceiling(123.456)
+
+create table[成績](
+	[学籍番号] int
+	,[氏名] nvarchar(40)
+	,[国語] int
+	,[数学] int
+	,[理科] int
+	,[社会] int
+	,[英語] int
+);
+
+insert into[成績]
+Values(1,'a',77,66,77,66,99);
+
+insert into[成績]
+Values(2,'b',77,66,77,66,99);
+
+insert into[成績]
+Values(3,'c',77,66,77,66,99);
+
+insert into[成績]
+Values(4,'d',77,66,77,66,99);
+
+insert into[成績]
+Values(5,'e',77,66,77,66,99);
